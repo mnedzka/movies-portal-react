fe14bd7976c0af433eb45b0ddbc7205d901f148a
Michal Nedzka <michael.nedzka@gmail.com>
2019-02-19 21:48:42 +0100

    Add navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
-import React, { Component } from "react";
-import Movies from "./components/Movies";
+import React, { Component } from 'react';
+import NavBar from './components/navbar';
+
+import Movies from './components/Movies';
 
 class App extends Component {
+  state = {
+    counters: [{ id: 1, value: 4 }, { id: 2, value: 0 }, { id: 3, value: 0 }, { id: 4, value: 0 }],
+  };
+
+  handleIncrement = counter => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
+  handleDecrement = counter => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value--;
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
+  handleDelete = counterId => {
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({ counters });
+  };
+
   render() {
     return (
-      <div className="container">
-        <Movies />
-      </div>
+      <React.Fragment>
+        <NavBar totalCounters={this.state.counters.filter(c => c.value > 0).length} />
+        <main className='container'>
+          <Movies />
+        </main>
+      </React.Fragment>
     );
   }
 }
