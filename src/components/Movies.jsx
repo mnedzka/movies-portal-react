2063d375e55d5951272236fa320ad6e438ca850c
Michal Nedzka <michael.nedzka@gmail.com>
2019-02-24 19:36:53 +0100

    Add last part of pagination

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { getMovies } from '../services/fakeMovieService';
 import Like from './common/Like';
 import Pagination from './common/Pagination';
+import { paginate } from '../utils/paginate';
 
 class Movies extends Component {
   state = {
@@ -32,9 +33,11 @@ class Movies extends Component {
 
   render() {
     const { length: count } = this.state.movies;
-    const { pageSize, currentPage } = this.state;
+    const { pageSize, currentPage, movies: allMovies } = this.state;
 
     if (count === 0) return <p>There are no movies in the database</p>;
+
+    const movies = paginate(allMovies, currentPage, pageSize);
     return (
       <>
         <p>Showing {count} movies in the database</p>
@@ -50,7 +53,7 @@ class Movies extends Component {
             </tr>
           </thead>
           <tbody>
-            {this.state.movies.map(movie => {
+            {movies.map(movie => {
               return (
                 <tr key={movie._id}>
                   <td>{movie.title}</td>
