34565f94ee2e977c61c4e4207f796e9922364fa7
Michal Nedzka <michael.nedzka@gmail.com>
2019-03-05 21:55:17 +0100

    Play with ref and controlled elements to check the best solution

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,14 +1,43 @@
 import React, { Component } from 'react';
 
 class LoginForm extends Component {
+  state = {
+    account: { username: '', pasword: '' },
+  };
+
+  username = React.createRef();
+
+  componentDidMount() {
+    this.username.current.focus();
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const usernmae = this.username.current.value;
+  };
+
+  handleChange = e => {
+    const account = { ...this.state.account };
+    account.username = e.currentTarget.value;
+    this.setState({ account });
+  };
+
   render() {
     return (
       <div>
         <h1>LoginForm</h1>
-        <form>
+        <form onSubmit={this.handleSubmit()}>
           <div className='form-group'>
             <label htmlFor='username'>Username</label>
-            <input id='username' type='text' className='form-control' />
+            <input
+              value={this.state.account.username}
+              onChange={this.handleChange()}
+              autoFocus
+              ref={this.username}
+              id='username'
+              type='text'
+              className='form-control'
+            />
           </div>
           <div className='form-group'>
             <label htmlFor='password'>Password</label>
