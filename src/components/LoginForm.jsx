f613039446f9d3f6d5ad543762ffd86445e4a237
Michal Nedzka <michael.nedzka@gmail.com>
2019-03-06 19:08:16 +0100

    Add basic validation

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -4,36 +4,70 @@ import Input from './common/Input';
 class LoginForm extends Component {
   state = {
     account: { username: '', password: '' },
+    errors: {},
+  };
+
+  validate = () => {
+    const errors = {};
+
+    const { account } = this.state;
+
+    if (account.username.trim() === '') errors.username = 'Username is required';
+
+    if (account.password.trim() === '') errors.password = 'Password is required';
+
+    return Object.keys(errors).length === 0 ? null : errors;
   };
 
   handleSubmit = e => {
     e.preventDefault();
+
+    const errors = this.validate();
+    this.setState({ errors: errors || {} });
+    if (errors) return;
     // const username = this.username.current.value;
   };
 
+  validateProperty = ({ name, value }) => {
+    if (name === 'username') {
+      if (value.trim() === '') return 'Username is required';
+    }
+    if (name === 'password') {
+      if (value.trim() === '') return 'Password is required';
+    }
+  };
+
   handleChange = ({ currentTarget: input }) => {
+    const errors = { ...this.state.errors };
+    const errorMessage = this.validateProperty(input);
+
+    if (errorMessage) errors[input.name] = errorMessage;
+    else delete errors[input.name];
+
     const account = { ...this.state.account };
     account[input.name] = input.value;
-    this.setState({ account });
+    this.setState({ account, errors });
   };
 
   render() {
-    const { account } = this.state;
+    const { account, errors } = this.state;
     return (
       <div>
-        <h1>LoginForm</h1>
-        <form onSubmit={this.handleSubmits}>
+        <h1>Login</h1>
+        <form onSubmit={this.handleSubmit}>
           <Input
             name='username'
             value={account.value}
             label='Username'
             onChange={this.handleChange}
+            error={errors.username}
           />
           <Input
             name='password'
             value={account.password}
             label='Password'
             onChange={this.handleChange}
+            error={errors.password}
           />
           <button className='btn btn-primary'>Login</button>
         </form>
--- src/components/common/NavbarRouter.jsx
+++ src/components/common/NavbarRouter.jsx
@@ -4,33 +4,33 @@ import { NavLink } from 'react-router-dom';
 const NavBarRouter = () => {
   return (
     <nav className='navbar navbar-router navbar-expand-lg navbar-light bg-light'>
-      <ul class='navbar-nav '>
-        <li class='nav-item'>
+      <ul className='navbar-nav '>
+        <li className='nav-item'>
           <NavLink className='nav-link' to='/'>
             Home
           </NavLink>
         </li>
-        <li class='nav-item'>
+        <li className='nav-item'>
           <NavLink className='nav-link' to='/counters'>
             Counters
           </NavLink>
         </li>
-        <li class='nav-item'>
+        <li className='nav-item'>
           <NavLink className='nav-link' to='/movies'>
             Movies
           </NavLink>
         </li>
-        <li class='nav-item'>
+        <li className='nav-item'>
           <NavLink className='nav-link' to='/customers'>
             Customers
           </NavLink>
         </li>
-        <li class='nav-item'>
+        <li className='nav-item'>
           <NavLink className='nav-link' to='/rentals'>
             Rentals
           </NavLink>
         </li>
-        <li class='nav-item'>
+        <li className='nav-item'>
           <NavLink className='nav-link' to='/login'>
             Login
           </NavLink>
